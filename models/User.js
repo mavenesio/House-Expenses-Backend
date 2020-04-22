b26ad757042c17ab11cb799417357e52223374f6
mariano_venesio <mvenesio@unitedvirtualities.com>
2020-04-21 22:47:00 -0300

    add user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique:true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+});
+
+
+module.exports = mongoose.model('User', UserSchema);
